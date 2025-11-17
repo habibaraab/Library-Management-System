28ba181da709175c3113d14cd73be82ce15a1a49
habibaraab <habibaragab324@gmail.com>
2025-11-17 21:13:21 +0200

    Add Dependancies and Configuration of project

---
+++ src/main/java/com/spring/libarary_management_system/LibararyManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.spring.libarary_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibararyManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibararyManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/libarary_management_system/LibararyManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.libarary_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibararyManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
