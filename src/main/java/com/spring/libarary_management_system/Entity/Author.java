ce056cb49a554d4125b9c602c0a78dc97d5958f1
habibaraab <habibaragab324@gmail.com>
2025-11-24 21:59:22 +0200

    (Add Cloudinary Config,Image Service  and , Entities :Book,Publisher,Author,Category )

---
+++ src/main/java/com/spring/libarary_management_system/Entity/Author.java
@@ -0,0 +1,49 @@
+package com.spring.libarary_management_system.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Entity
+@Table(name = "authors")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long authorId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String bio;
+
+    @Column(unique = true)
+    private String email;
+
+    @ManyToMany(mappedBy = "authors")
+    @Builder.Default
+    private List<Book> books = new ArrayList<>();
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
